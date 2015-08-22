fe6ffde91da4d9a110ffed94403360ecdc59eb7f
David Douglas <david@deadlyfingers.com>
2015-08-22 17:49:57 +0100

    Windows app support added. Resolves #2

--- Assets/MobileServices/http/ZumoRequest.cs
+++ Assets/MobileServices/http/ZumoRequest.cs
@@ -14,7 +14,7 @@ namespace Unity3dAzure.MobileServices
         public ZumoRequest(MobileServiceClient client, string uri, Method httpMethod) : base(uri, httpMethod)
         {
             this.AddHeader("X-ZUMO-APPLICATION", client.AppKey);
-            this.AddHeader("Content-Type", "application/json; charset=UTF-8");
+            //this.AddHeader("Content-Type", "application/json; charset=UTF-8"); // this line caused winrt app to not connect
             this.AddHeader("Accept", "application/json");
             this.RequestFormat = DataFormat.Json;
             if (client.User != null && !string.IsNullOrEmpty(client.User.authenticationToken))
