73ed30cd5140c8709d24d0d7d151b5e0d9b0c6fd
David Douglas <david@deadlyfingers.com>
2016-08-31 19:09:06 +0100

    :sparkles: Using new Unity UI with TSTableView. Resolves #3

---
+++ Assets/Scripts/HighscoresDemo.cs
@@ -0,0 +1,425 @@
+﻿using UnityEngine;
+using System;
+using System.Net;
+using System.Collections.Generic;
+using RestSharp;
+using Pathfinding.Serialization.JsonFx;
+using Unity3dAzure.AppServices;
+using UnityEngine.UI;
+using Tacticsoft;
+
+[CLSCompliant(false)]
+public class HighscoresDemo : MonoBehaviour, ITableViewDataSource
+{
+	/// <remarks>
+	/// Enter your Azure App Service connection strings
+	/// </remarks>
+
+	[Header("Azure App Service")]
+	/// Azure Mobile App connection strings
+	[SerializeField]
+	private string _appUrl = "PASTE_YOUR_APP_URL";
+
+	/// Go to https://developers.facebook.com/tools/accesstoken/ to generate a new access "User Token"
+	private string _facebookAccessToken;
+
+	/// App Service Rest Client
+	private MobileServiceClient _client;
+
+	/// App Service Table defined using a DataModel
+	private MobileServiceTable<Highscore> _table;
+
+	/// List of highscores (leaderboard)
+	private List<Highscore> _scores = new List<Highscore>();
+
+	private Highscore _score;
+
+	[Header("UI")]
+	// TSTableView for displaying list of results
+	[SerializeField]
+	private TableView _tableView;
+	[SerializeField]
+	private ScoreCell _cellPrefab;
+	bool HasNewData = false;
+
+	/// Use this for initialization
+	void Start ()
+	{
+		/// Create App Service client (Using factory Create method to force 'https' url)
+		_client = MobileServiceClient.Create(_appUrl); //new MobileServiceClient(_appUrl);
+
+		/// Get App Service 'Highscores' table
+		_table = _client.GetTable<Highscore>("Highscores");
+
+		// set TSTableView delegate
+		_tableView.dataSource = this;
+
+		UpdateUI();
+	}
+
+	/// Update is called once per frame
+	void Update () 
+	{
+		// Only update table when there is new data
+		if (HasNewData) {
+			Debug.Log ("Refresh Table Data");
+			_tableView.ReloadData ();
+			HasNewData = false;
+		}
+		// Display new score details 
+		if (_score != null) {
+			Debug.Log ("Show score");
+			DisplayScore (_score);
+			_score = null;
+		}
+	}
+
+	public void Login()
+	{
+		_client.Login(MobileServiceAuthenticationProvider.Facebook, _facebookAccessToken, OnLoginCompleted);
+	}
+
+	private void OnLoginCompleted(IRestResponse<MobileServiceUser> response)
+	{
+		Debug.Log("Status: " + response.StatusCode + " Uri:" + response.ResponseUri );
+		Debug.Log("OnLoginCompleted: " + response.Content );
+
+		if ( response.StatusCode == HttpStatusCode.OK)
+		{
+			MobileServiceUser mobileServiceUser = response.Data;
+			_client.User = mobileServiceUser;
+			Debug.Log("Authorized UserId: " + _client.User.user.userId );
+		}
+		else
+		{
+			Debug.Log("Authorization Error: " + response.StatusCode);
+		}
+	}
+
+	public void Insert()
+	{
+		Highscore score = GetScore ();
+		if ( Validate(score) )
+		{
+			_table.Insert<Highscore>(score, OnInsertCompleted);
+		}
+	}
+
+	private void OnInsertCompleted(IRestResponse<Highscore> response)
+	{
+		if (response.StatusCode == HttpStatusCode.Created)
+		{
+			Debug.Log( "OnInsertItemCompleted: " + response.Data );
+        	Highscore item = response.Data; // if successful the item will have an 'id' property value
+			_score = item;
+		}
+		else
+		{
+			Debug.Log("Insert Error Status:" + response.StatusCode + " Uri: "+response.ResponseUri );
+		}
+	}
+
+	public void UpdateScore()
+	{
+		Highscore score = GetScore ();
+		if ( Validate(score) )
+		{
+			_table.Update<Highscore>(score, OnUpdateScoreCompleted);
+		}
+	}
+
+	private void OnUpdateScoreCompleted(IRestResponse<Highscore> response)
+	{
+		if (response.StatusCode == HttpStatusCode.OK)
+		{
+			Debug.Log("OnUpdateItemCompleted: " + response.Content );
+		}
+		else
+		{
+			Debug.Log("Update Error Status:" + response.StatusCode +" "+ response.ErrorMessage + " Uri: "+response.ResponseUri );
+		}
+	}
+
+	public void Delete()
+	{
+		Highscore score = GetScore ();
+		_table.Delete<Highscore>(score.id, OnDeleteCompleted);
+	}
+
+	private void OnDeleteCompleted(IRestResponse<Highscore> response)
+	{
+		if (response.StatusCode == HttpStatusCode.OK)
+		{
+			Debug.Log("OnDeleteItemCompleted");
+		}
+		else
+		{
+			Debug.Log("Delete Error Status:" + response.StatusCode +" "+ response.ErrorMessage + " Uri: "+response.ResponseUri );
+		}
+	}
+
+	public void Read()
+	{
+		_table.Read<Highscore>(OnReadCompleted);
+	}
+
+	private void OnReadCompleted(IRestResponse<List<Highscore>> response)
+	{
+		if (response.StatusCode == HttpStatusCode.OK)
+		{
+			Debug.Log("OnReadItemsCompleted data: " + response.Content);
+        	List<Highscore> items = response.Data;
+        	Debug.Log("Read items count: " + items.Count);
+			_scores = items;
+			HasNewData = true;
+		}
+		else
+		{
+			Debug.Log("Read Error Status:" + response.StatusCode + " Uri: "+response.ResponseUri );
+		}
+	}
+
+	public void GetAllHighscores()
+	{
+		CustomQuery query = CustomQuery.OrderBy ("score desc");
+		Query(query);
+	}
+
+	public void GetTopHighscores()
+	{
+		DateTime today = DateTime.Today;
+		string day = today.ToString("s");
+		string filter = string.Format("createdAt gt '{0}Z'", day); //string.Format("score gt {0}", 999);
+		Debug.Log ("filter:" + filter);
+		string orderBy = "score desc";
+		CustomQuery query = new CustomQuery(filter,orderBy,10);
+		Query(query);
+	}
+
+	public void GetUsernameHighscore()
+	{
+		Highscore score = GetScore ();
+		string filter = string.Format("username eq '{0}'", score.username); // string.Format("startswith(username,'{0}')", score.username);
+		string orderBy = "score desc";
+		CustomQuery query = new CustomQuery(filter,orderBy);
+		Query(query);
+	}
+
+	private void Query(CustomQuery query)
+	{
+		_table.Query<Highscore>(query, OnReadCompleted);
+	}
+
+	/// <summary>
+	/// This demo 'hello' custom api just gets a response message '{"message":"Hello World!"}'
+	/// </summary>
+	public void Hello()
+	{
+		_client.InvokeApi<Message>("hello", OnCustomApiCompleted);
+	}
+
+	public void GenerateScores()
+	{
+		_client.InvokeApi<Message>("GenerateScores", OnCustomApiCompleted);
+	}
+
+	private void OnCustomApiCompleted(IRestResponse<Message> response)
+	{
+		if (response.StatusCode == HttpStatusCode.OK)
+		{
+			Debug.Log("OnCustomApiCompleted data: " + response.Content);
+       		Message message = response.Data;
+			Debug.Log( "Result: " + message);
+		}
+		else
+		{
+			Debug.Log("Api Error Status:" + response.StatusCode + " Uri: "+response.ResponseUri );
+		}
+	}
+
+
+	/// <summary>
+	/// This is an example showing how to get an item using it's id.
+	/// </summary>
+	private void Lookup()
+	{
+		Highscore score = GetScore ();
+		_table.Lookup<Highscore>(score.id, OnLookupCompleted);
+	}
+
+	private void OnLookupCompleted(IRestResponse<Highscore> response)
+	{
+		Debug.Log("OnLookupItemCompleted: " + response.Content );
+		if (response.StatusCode == HttpStatusCode.OK)
+		{
+			Highscore item = response.Data;
+			_score = item;
+		}
+		else
+		{
+			ResponseError err = JsonReader.Deserialize<ResponseError>(response.Content);
+			Debug.Log("Lookup Error Status:" + response.StatusCode + " Code:" + err.code.ToString() + " " + err.error);
+		}
+	}
+
+	/// <summary>
+	/// Create data model from UI
+	/// </summary>
+	private Highscore GetScore() 
+	{
+		string name = GameObject.Find("InputName").GetComponent<InputField> ().text;
+		string score = GameObject.Find("InputScore").GetComponent<InputField> ().text;
+		string id = GameObject.Find("Id").GetComponent<Text> ().text;
+		Highscore highscore = new Highscore();
+		highscore.username = name;
+		if (!String.IsNullOrEmpty (score)) {
+			highscore.score = Convert.ToInt32 (score);
+		}
+		if (!String.IsNullOrEmpty (id)) {
+			highscore.id = id;
+			Debug.Log ("Existing Id:" + id);
+		}
+		return highscore;
+	}
+
+	/// <summary>
+	/// Update UI with data model 
+	/// </summary>
+	private void DisplayScore(Highscore highscore) 
+	{
+		InputField name = GameObject.Find("InputName").GetComponent<InputField> ();
+		InputField score = GameObject.Find("InputScore").GetComponent<InputField> ();
+		Text id = GameObject.Find("Id").GetComponent<Text> ();
+		name.text = highscore.username;
+		score.text = highscore.score.ToString ();
+		id.text = highscore.id;
+		UpdateUI ();
+	}
+
+	/// <summary>
+	/// Reset UI to insert new score
+	/// </summary>
+	public void ClearScore() {
+		InputField name = GameObject.Find("InputName").GetComponent<InputField> ();
+		InputField score = GameObject.Find("InputScore").GetComponent<InputField> ();
+		Text id = GameObject.Find("Id").GetComponent<Text> ();
+		name.text = "";
+		score.text = "";
+		id.text = "";
+		UpdateUI ();
+	}
+
+	/// <summary>
+	/// Validate data before sending
+	/// </summary>
+	private bool Validate(Highscore highscore)
+	{
+		bool isUsernameValid=true, isScoreValid=true;
+		// Validate username
+		if (String.IsNullOrEmpty (highscore.username)) {
+			isUsernameValid = false;
+			Debug.Log ("Error, player username required");
+		}
+		// Validate score
+		if ( !(highscore.score > 0) )
+		{
+			isScoreValid = false;
+			Debug.Log ("Error, player score should be greater than 0");
+		}
+		UpdateText ("Player", isUsernameValid);
+		UpdateText ("Score", isScoreValid);
+		return (isUsernameValid && isScoreValid);
+	}
+
+	/// <summary>
+	/// Change text color to highlight errors
+	/// </summary>
+	private void UpdateText(string gameObjectName, bool isValid=true)
+	{
+		Text text = GameObject.Find(gameObjectName).GetComponent<Text> ();
+		if (text) {
+			text.color = isValid ? Color.white : Color.red;
+		}
+	}
+
+	/// <summary>
+	/// Handler for text changed event to update view state
+	/// </summary>
+	public void TextChanged() 
+	{
+		UpdateUI ();
+	}
+
+	/// <summary>
+	/// Method to manage UI view state
+	/// </summary>
+	private void UpdateUI() 
+	{
+		// Activate login button if Facebook Access Token is entered
+		Button login = GameObject.Find("Login").GetComponent<Button> ();
+		_facebookAccessToken = GameObject.Find("FacebookAccessToken").GetComponent<InputField> ().text;
+		login.interactable = String.IsNullOrEmpty (_facebookAccessToken) ? false : true ;
+
+		// Insert or Update mode
+		Text id = GameObject.Find("Id").GetComponent<Text> ();
+		GameObject insert = GameObject.Find("GroupInsert");
+		GameObject update = GameObject.Find("GroupUpdate");
+		CanvasGroup groupInsert = insert.GetComponent<CanvasGroup> ();
+		CanvasGroup groupUpdate = update.GetComponent<CanvasGroup> ();
+		if ( String.IsNullOrEmpty(id.text) )
+		{
+			groupInsert.alpha = 1; 
+			groupUpdate.alpha = 0;
+			groupInsert.interactable = true;
+			groupUpdate.interactable = false;
+		} 
+		else 
+		{
+			groupInsert.alpha = 0; 
+			groupUpdate.alpha = 1;
+			groupInsert.interactable = false;
+			groupUpdate.interactable = true;
+		}
+	}
+
+	#region ITableViewDataSource
+
+	public int GetNumberOfRowsForTableView(TableView tableView)
+	{
+		return _scores.Count;
+	}
+
+	public float GetHeightForRowInTableView(TableView tableView, int row)
+	{
+		return (_cellPrefab.transform as RectTransform).rect.height; //50.0f;
+	}
+
+	public TableViewCell GetCellForRowInTableView(TableView tableView, int row)
+	{
+		ScoreCell cell = tableView.GetReusableCell(_cellPrefab.reuseIdentifier) as ScoreCell;
+		if (cell == null) {
+			cell = (ScoreCell)GameObject.Instantiate (_cellPrefab);
+		}
+		Highscore data = _scores [row];
+		cell.Name.text = data.username;
+		cell.Score.text = data.score.ToString();
+		cell.Rank.text = (row+1).ToString();
+		cell.Btn.name = row.ToString(); // save index to button name
+		return cell;
+	}
+
+	#endregion
+
+	/// <summary>
+	/// Handler to get selected row item
+	/// </summary>
+	public void OnSelectedRow(Button button) {
+		int index = Convert.ToInt32 (button.name);
+		//Debug.Log("Selected index:" + index);
+		if (index >= _scores.Count) {
+			return;
+		}
+		Highscore score = _scores [index];
+		_score = score;
+	}
+}
--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Sample highscore leaderboard project demoing Azure App Services (previously Mobi
 * Client-directed login with Facebook
 * Insert Highscore
 * Update Highscore
-* Read list of Highscores
-* Query for scores > 999 (leaderboard)
+* Read list of Highscores (hall of fame)
+* Query for today's top ten highscores (daily leaderboard)
 * Query for username (user's scores)
 
 ## Developer guide
@@ -14,22 +14,23 @@ Read developer guide on [using Azure App Services to create Unity highscores lea
 
 ## Setup Azure App Services for Unity
 1. Create an [Azure Mobile App](https://portal.azure.com/)
-	* Create 'Highscores' table for app data
-	* Modify 'Highscores' table Insert node script to save userId
-	* Create a custom API called 'hello'
-2. In Unity3d **open scene** `Scenes/AppServicesDemo.unity`
-	* Check the script `Scripts/AppServicesDemo.cs` is attached to a game object in the Unity Hierarchy window.
-3. Paste Azure Mobile Service app's connection strings into Unity Editor Inspector fields
-	![alt Unity Editor Mobile Services config](https://cloud.githubusercontent.com/assets/1880480/14404802/c82512da-fe76-11e5-91ad-316fcd70fd5c.png)
-	* App Service URL (NB: use https)
-4. If you want to save score with userId then [create Facebook app](https://developers.facebook.com/apps/)
-	* Fill in Azure Mobile Service's Identity > Facebook settings (App Id & App Secret)
+	* Create 'Highscores' table for storing app data using **Easy Tables**.
+2. In Unity open scene "Scenes/HighscoresDemo.unity"
+3. Select the *AppServicesController gameobject* in the Unity Hierarchy window and paste your **Azure App Service URL** into the Editor Inspector field.
+	![alt Unity Editor Mobile Services config](https://cloud.githubusercontent.com/assets/1880480/18139855/0e5fe626-6fab-11e6-8de6-484e3b909cc8.png)
+4. If you wish to save score using Facebook identity:
+	* [Create Facebook app](https://developers.facebook.com/apps/)
+	* Fill in the Azure App Services Authentication settings for Facebook. (Facebook App Id & App Secret required)
 	* Paste [Facebook access user token](https://developers.facebook.com/tools/accesstoken/) into Unity Editor Inspector field
-	* Play in UnityEditor
+	* Modify 'Highscores' table 'Insert' node script (using snippet below) to save `userId`
+5. Create optional custom APIs using **Easy APIs**. (Example snippets below)
+	* Create a 'hello' api to say hello!
+	* Create a 'GenerateScores' api to generate 10 random scores.
 
-### Azure App Services
-#### Highscores Table **Insert** script
+### Easy Table scripts
+#### 'tables/Highscores.js' **Insert** script
 ```node
+var table = module.exports = require('azure-mobile-apps').table();
 table.insert(function (context) {
 	if (context.user) {
 		context.item.userId = context.user.id;
@@ -37,6 +38,46 @@ table.insert(function (context) {
 	return context.execute();
 });
 ```
+
+### Easy API scripts
+#### 'api/hello.js' script
+```node
+module.exports = {
+    "get": function (req, res, next) {
+        res.send(200, { message : "Hello Unity!" });
+    }
+}
+```
+
+#### 'api/GenerateScores.js' script
+```node
+var util = require('util');
+module.exports = {
+    "get": function (req, res, next) {
+        var insert = "INSERT INTO Highscores (username,score) VALUES ";
+        var i = 10;
+        while (i--) {
+            var min = 1;
+            var max = 1000;
+            var rand = Math.floor(Math.random() * (max - min)) + min;
+            var values = util.format("('%s',%d),", 'Zumo', rand);
+            insert = insert + values;
+        }
+        insert = insert.slice(0, -1); // remove last ','
+        var query = {
+            sql: insert
+        };
+        req.azureMobile.data.execute(query).then(function(results){
+            res.send(200, { message : "Zumo set some highscores!" });
+        });
+    }
+}
+
+```
+
+## Dependencies included
+* [AppService](https://github.com/Unity3dAzure/AppServices) for Unity implements [UnityRestClient](https://github.com/ProjectStratus/UnityRestClient) which uses [JsonFx](https://bitbucket.org/TowerOfBricks/jsonfx-for-unity3d-git/) to parse JSON data.
+* [TSTableView](https://bitbucket.org/tacticsoft/tstableview) is used to display recyclable list of results.
 
 ## Supports
 * iOS
